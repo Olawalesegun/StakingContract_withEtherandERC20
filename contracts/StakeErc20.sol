d98c55d2051451020aee06729a84aa9e58a0dea4
olawalesegun <olawalesegun0@gmail.com>
2024-08-31 17:00:16 +0300

    updated the ERC20 contract with withdraw and a reward mechanism

--- contracts/StakeErc20.sol
+++ contracts/StakeErc20.sol
@@ -24,10 +24,13 @@ contract StakeERC20 {
 
     mapping(address => Stakes) balances;
 
-    error NotSufficientAMountToStake();
     error AddressZeroDetected();
+    error NotSufficientAMountToStake();
     error MinimumStakingDurationIs92();
     error InSufficientAmountToStake();
+    error ThisUserDoesNotExist();
+    error NoStakeFound();
+    error StakeNotMatured();
 
     function stakeErc20(uint256 _amount, uint256 _duration) payable  public{
         if(msg.sender == address(0)) {
@@ -56,5 +59,53 @@ contract StakeERC20 {
             beginningOfStakedDate: block.timestamp,
             endOfStakeDate: block.timestamp + _duration * 1 days
         });
+    }
+
+
+    function calcInterest(address staker) private view returns (uint256) {
+        Stakes memory userStake = balances[staker];
+        uint256 durationStaked;
+
+        if (userStake.amountStaked == 0) {
+            revert ThisUserDoesNotExist();
+        }
+
+        if (userStake.beginningOfStakedDate > userStake.endOfStakeDate) {
+            revert("Invalid staking dates");
+        }
+
+        if (block.timestamp > userStake.endOfStakeDate) {
+            durationStaked = userStake.endOfStakeDate - userStake.beginningOfStakedDate;
+        } else {
+            durationStaked = block.timestamp - userStake.beginningOfStakedDate;
+        }
+
+
+        uint256 ratePerDay = 1;
+        uint256 interest = (userStake.amountStaked * ratePerDay * durationStaked) / (100 * 1 days);
+
+        return interest;
+    }
+
+    function withdraw() public {
+        Stakes storage userStake = balances[msg.sender];
+
+        if (userStake.amountStaked == 0) {
+            revert NoStakeFound();
+        }
+
+        if (block.timestamp < userStake.endOfStakeDate) {
+            revert StakeNotMatured();
+        }
+
+        uint256 interest = calcInterest(msg.sender);
+        uint256 totalAmount = userStake.amountStaked + interest;
+
+        userStake.amountEarnedFromStaking += interest;
+        userStake.amountStaked = 0;
+        userStake.beginningOfStakedDate = 0;
+        userStake.endOfStakeDate = 0;
+
+        IERC20(tokenAddress).transfer(msg.sender, totalAmount);
     }
 }
